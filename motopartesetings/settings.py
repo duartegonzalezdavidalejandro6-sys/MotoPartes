3b7e2e47e84316ca4490bbccf8d4b2fb63965a3e
duartegonzalezdavidalejandro6-sys <duartegonzalezdavidalejandro6@gmail.com>
2026-06-03 17:07:22 -0500

    Update settings.py

--- motopartesetings/settings.py
+++ motopartesetings/settings.py
@@ -166,4 +166,4 @@ CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
 CELERY_TIMEZONE = "America/Bogota"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
