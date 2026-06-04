e368af6b3604bf6c3f4a3d0395c9de452f37c1c0
xxcyeyexxc <148709280+xxcyeyexxc@users.noreply.github.com>
2026-06-04 02:12:41 -0500

    Update settings.py

--- motopartesetings/settings.py
+++ motopartesetings/settings.py
@@ -118,7 +118,7 @@ MEDIA_URL = '/'
 # ══════════════════════════════════════════
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.mailersend.net'
-EMAIL_PORT = 587
+EMAIL_PORT = 2525
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'MS_RMolyy@test-r6ke4n1o1ovgon12.mlsender.net'
 EMAIL_HOST_PASSWORD = 'mlsn.c2f60531dee8f31b6c2ed1349470c5e5734c4a0b89256a5ef93d4ce0df18e5e5'
